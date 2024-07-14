47d01dfdfbf9ce3086cee03aa88a11552da6c3c1
platiins <valerijaaivanovaa@gmail.com>
2024-07-14 06:50:08 +0300

    adding more icon with animation

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const titles = document.querySelectorAll(".toggle-title");
 titles.forEach((title) => {
   title.addEventListener("click", () => {
     const description = title.nextElementSibling;
+    const iconMore = title.querySelector(".icon-more");
     if (description.classList.contains("toggle-description")) {
       description.classList.toggle("active");
+      iconMore.classList.toggle("active");
     }
   });
 });
